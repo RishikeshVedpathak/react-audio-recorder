0503d3af97faa4d1bd751c24809506312aca4665
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-06-07 02:50:26 +0530

    example to show component

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,16 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import AudioRecorder from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <AudioRecorder
+        onGenerateAudioURL={audioUrl =>
+          console.log('Generated Audio URL', audioUrl)
+        }
+      />
     </div>
   );
 };
